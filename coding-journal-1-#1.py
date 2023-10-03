b17dbb87f0ef6249fd086c276e96e61fd2d000f9
Mira Prabhakar <70825360+miraprabhakar@users.noreply.github.com>
2023-10-03 12:52:52 -0700

    Add files via upload

---
+++ coding-journal-1-#1.py
@@ -0,0 +1,10 @@
+def Introduction():
+	print("Hi! My name is Mira Prabhakar! I use she/her pronouns.
+	My favorite movie is Howl's moving castle and my favorite 
+	food are dumplings.")
+
+def main():
+	Introduction()
+
+if _name_=="_main_":
+	main()
