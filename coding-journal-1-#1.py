3502d0aec5cee84276ee087be084bd371d62bacc
Mira Prabhakar <70825360+miraprabhakar@users.noreply.github.com>
2023-10-03 16:38:19 -0700

    Update coding-journal-1-#1.py

--- coding-journal-1-#1.py
+++ coding-journal-1-#1.py
@@ -1,10 +1,9 @@
 def Introduction():
-	print("Hi! My name is Mira Prabhakar! I use she/her pronouns.
-	My favorite movie is Howl's moving castle and my favorite 
-	food are dumplings.")
+	print("Hi! My name is Mira Prabhakar! \nI use she/her pronouns. \nMy favorite movie is Howl's moving castle and my favorite food are dumplings.")
 
 def main():
 	Introduction()
 
-if _name_=="_main_":
+if __name__=="__main__":
 	main()
+
