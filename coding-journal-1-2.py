d4288fc13730e8bf23a6708a9e332d1dc0ed15a5
Mira Prabhakar <70825360+miraprabhakar@users.noreply.github.com>
2023-10-05 11:59:44 -0700

    Add files via upload

---
+++ coding-journal-1-2.py
@@ -0,0 +1,8 @@
+# this program defines multiple expressions and prints them
+
+sum1 = float(1.2)
+sum2 = float(1.3)
+int_sum = 3 - 2
+product = float(1.2) * 2
+
+print ((sum1*sum2), int_sum, product)
